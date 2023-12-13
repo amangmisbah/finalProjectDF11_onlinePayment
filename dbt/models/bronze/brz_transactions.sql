6720c1a7568045bad4494b7df5bdc782467f6b79
risw24 <riswanda.alifarahman@gmail.com>
2023-12-13 14:01:20 +0700

    add schema and refactor bronze

--- dbt/models/bronze/brz_transactions.sql
+++ dbt/models/bronze/brz_transactions.sql
@@ -1,24 +1,22 @@
-WITH NumberedRows AS (
+WITH source AS (
     SELECT
-        *,
-        ROW_NUMBER() OVER (ORDER BY payment_datetime) AS row_num
-    FROM {{ ref('brz_transactions_merge') }}
+        *
+    FROM {{ source('brz_online_payment', 'brz_transaction') }}
 )
 
 SELECT
-    CONCAT('P', LPAD(CAST(row_num AS STRING), 10, '0'), '_', FORMAT_TIMESTAMP('%Y%m%d%H%M%S', payment_datetime)) AS transactionID,
-    payment_datetime as transactionDatetime,
+    idTransaction as transactionID,
+    timestamp as transactionDatetime,
     type,
     ROUND(amount, 2) as amount,
-    nameOrig,
-    ROUND(oldbalanceOrg, 2) as oldBalanceOrig,
-    ROUND(newbalanceOrig, 2) as newBalanceOrig,
+    nameOrigin,
+    ROUND(oldBalanceOrigin, 2) as oldBalanceOrig,
+    ROUND(newBalanceOrigin, 2) as newBalanceOrig,
     nameDest,
     ROUND(oldbalanceDest, 2) as oldBalanceDest,
     ROUND(newbalanceDest, 2) as newBalanceDest,
     CAST(isFraud as BOOLEAN) as isFraud,
-    CAST(isFlaggedFraud as BOOLEAN) as isFlaggedFraud,
-    step
+    CAST(isFlaggedFraud as BOOLEAN) as isFlaggedFraud
 FROM
-    NumberedRows
+    source
 ORDER BY transactionID
